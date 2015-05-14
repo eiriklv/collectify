a2ade9d1f19d73a53f9d635b8ea4aabdf89ac676
Eirik Vullum <evullum@gmail.com>
2015-05-15 00:42:09 +0200

    rename _ to lodash

--- helpers/format-for-update.js
+++ helpers/format-for-update.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const _ = require('lodash-fp');
+const lodash = require('lodash-fp');
 const asap = require('asap');
 const debug = require('debug')('format-for-updating');
 
 exports = module.exports = function(picks) {
   return function(obj, callback) {
-    asap(callback.bind(this, null, _.pick(picks, obj), obj))
+    asap(callback.bind(this, null, lodash.pick(picks, obj), obj))
   };
 };
